438b3f5f6884aa8e5e3c8f93bd1d0d31b0b88a6a
Rune Lain Knudsen <r.l.knudsen909@gmail.com>
2016-11-12 19:46:11 +0100

    Add some elements to the DOM.

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,8 @@
 console.log("Hello!");
+
+let div = document.createElement('div');
+let h1 = document.createElement('h1');
+h1.textContent = "Helloooo!";
+
+div.appendChild(h1);
+document.body.appendChild(div);
