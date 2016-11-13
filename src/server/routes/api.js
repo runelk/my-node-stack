b7522ac8ddf1f28f2d0e78f8f64c2f8ae42744fa
Rune Lain Knudsen <r.l.knudsen909@gmail.com>
2016-11-13 16:36:10 +0100

    Use Sequelize example models in REST api.

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router({mergeParams: true});
 
+var models = require('../database/sequelize/models');
+
 router.route('/')
   .get(function(req, res) {
     res.json({ message: 'Hello there!' });
@@ -8,7 +10,16 @@ router.route('/')
 
 router.route('/examples')
   .get(function(req, res) {
-    res.json({ message: 'Some examples here please!' });
+    models.Example.findAll().then(function(examples) {
+      res.json(examples);
+    });
+  });
+
+router.route('/examples/:id')
+  .get(function(req, res) {
+    models.Example.findById(req.params.id).then(function(example) {
+      res.json(example);
+    });
   });
 
 module.exports = router;
